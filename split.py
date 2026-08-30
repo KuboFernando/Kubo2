142eca37c8f5266db75783ed31b2de52e53fb47d
KuboFernando <fernandokubo@id.uff.br>
2026-08-30 20:38:33 -0300

    exercício split

--- split.py
+++ split.py
@@ -1,17 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-sx = 0  # source 
+sx = 120  # source 
 sy = 0
-
 d_rec = 12.5 # metros
-offset_min = 150 # metros
+offset_min = 0 # metros
 offset_max = 400 # metros
 
 rx_esq = np.arange(-offset_max, -offset_min + d_rec, d_rec)
 rx_dir = np.arange(offset_min, offset_max + d_rec, d_rec)
 
-rx = rx_dir + rx_esq
+rx = np.concatenate((rx_esq,rx_dir))
 
 ry = np.full(len(rx), sy)
 
@@ -22,6 +21,32 @@ plt.scatter(sx, sy, marker= '*', s = 200, label = 'Fonte')
 
 plt.xlabel('Distância')
 plt.ylabel('Y')
+plt.grid(True)
+plt.legend()
+
+
+offset = np.abs(rx - sx)
+
+v1 = 1500 
+v2 = 3000
+z = 1000
+
+x = np.arange(150, 8000, 12.5) # offsets
+
+t_refl = np.sqrt(offset**2 + 4*z**2)/v1
+
+
+theta = np.degrees(np.arcsin(v1/v2))
+t_refr = (offset/v2) + (2*z*np.cos(theta))/v1
+
+t_dir = offset / v1
+plt.figure(figsize=(12, 7))
+plt.plot(rx, t_dir, label="Onda direta")
+plt.plot(rx, t_refl, label="Onda refletida")
+plt.plot(rx, t_refr, label="Onda refratada")
+plt.xlabel("Offset (m)")
+plt.ylabel("Tempo (s)")
+
 plt.grid(True)
 plt.legend()
 
